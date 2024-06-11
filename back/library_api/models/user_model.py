2070ed7d25091ee71df1aa33a2c8f3d4d346326d
viniciusantunes26 <viniciusantunes29@gmail.com>
2024-06-10 21:41:45 -0300

    docs: added description of the methods in user_model

--- back/library_api/models/user_model.py
+++ back/library_api/models/user_model.py
@@ -1,8 +1,22 @@
 class User:
     def __init__(self, email, username, password):
+        """
+        Initialize a new User instance.
+
+        Args:
+            email (str): The email address of the user.
+            username (str): The username of the user.
+            password (str): The password of the user.
+        """
         self.email = email
         self.username = username
         self.password = password
 
     def __str__(self):
+        """
+        Return a string representation of the user.
+
+        Returns:
+            str: The username of the user.
+        """
         return self.username
