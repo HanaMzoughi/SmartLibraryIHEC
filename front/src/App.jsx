dd8e1033884f8c6ec66726fe7178fe683c2cd23c
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-06-14 19:34:00 -0300

    feat: create App file to integrate react router dom to navigate pages

---
+++ front/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginRegister from "./pages/loginRegister/LoginRegister";
+import Profile from "./pages/profile/Profile";
+import Bookshelf from "./pages/bookshelf/Bookshelf";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import "./App.css";
+import Book from "./pages/book/Book";
+
+const App = () => {
+  const [auth, setAuth] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, []);
+
+  const handleAuth = () => {
+    setAuth(!auth);
+  };
+
+  return (
+    <Router>
+      <div className="screen">
+        <Header className="app-header" auth={auth} />
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login-register"
+              element={<LoginRegister handleAuth={handleAuth} />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile handleAuth={handleAuth} />}
+            />
+            <Route path="/bookshelf" element={<Bookshelf auth={auth} />} />
+            <Route path="/:bookId" element={<Book />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
