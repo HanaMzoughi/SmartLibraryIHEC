839c0fea17bf7760c0c652cfa44b2488993c4a9e
YasmineRmedi <yasminermedi024@gmail.com>
2024-12-17 17:17:53 +0100

    backend Book

--- back/library_api/serializers/book_serializer.py
+++ back/library_api/serializers/book_serializer.py
@@ -1,7 +1,6 @@
 class BookSerializer:
     @staticmethod
     def serialize(book_data):
-
         """
         Serialize book data into a format suitable for API responses.
 
@@ -13,10 +12,29 @@ class BookSerializer:
         """
         
         serialized_book = {
-            'id': str(book_data['_id']),
-            'title': book_data['title'],
-            'author': book_data['author'],
-            'link': book_data['link'],
-            'post_by': book_data['post_by'],
+            'id': str(book_data['_id']),  # Convert ObjectId to string
+            'N': book_data.get('N', ''),
+            'BIBID': book_data.get('BIBID', ''),
+            'ITEMID': book_data.get('ITEMID', ''),
+            'Code_barre': book_data.get('Code_barre', ''),
+            'D_Object': book_data.get('D_Object', ''),
+            'CREATION': book_data.get('CREATION', ''),
+            'MODIF': book_data.get('MODIF', ''),
+            'Cote': book_data.get('Cote', ''),
+            'Inventaire': book_data.get('Inventaire', ''),
+            'epn': book_data.get('epn', ''),
+            'Titre': book_data.get('Titre', ''),
+            'Auteur': book_data.get('Auteur', ''),
+            'Locale': book_data.get('Locale', ''),
+            'Staff_Note': book_data.get('Staff_Note', ''),
+            'Public_Note': book_data.get('Public_Note', ''),
+            'ISBN_A': book_data.get('ISBN_A', ''),
+            'ISBN_Z': book_data.get('ISBN_Z', ''),
+            'Item_class': book_data.get('Item_class', ''),
+            'Specialite': book_data.get('Specialite', ''),
+            'Nb_Page': book_data.get('Nb_Page', ''),
+            'Date_edition': book_data.get('Date_edition', ''),
+            'Editeur': book_data.get('Editeur', ''),
+            'Prix': book_data.get('Prix', ''),
         }
         return serialized_book
