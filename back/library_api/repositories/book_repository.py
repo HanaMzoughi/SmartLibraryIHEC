1918b6f7ba40d929a8223780e23424bbe95ee3e1
YasmineRmedi <yasminermedi024@gmail.com>
2024-12-19 18:51:16 +0100

    Résolution des conflits

--- back/library_api/repositories/book_repository.py
+++ back/library_api/repositories/book_repository.py
@@ -1,30 +1,6 @@
 from library_project.settings import *
 from bson.objectid import ObjectId
 class BookRepository:
-<<<<<<< HEAD
- def __init__(self):
-
-   self.client = client
-   self.db = db
-   self.collection = self.db['biblio']
-
- def create_book(self, book_data):
-
-   result = self.collection.insert_one(book_data)
-   return result.acknowledged
-
- def get_book(self, _id):
-
-   book_data = self.collection.find_one({'_id': ObjectId(_id)})
-   return book_data
-
- def get_books(self):
-
-    book_data = self.collection.find()
-    return book_data
-
- def update_book(self, _id, update_data):
-=======
     def __init__(self):
         """
         Initialize a new BookRepository instance.
@@ -50,18 +26,10 @@ class BookRepository:
     def get_book(self, _id):
         """
         Retrieve a book entry from the database by its ID.
->>>>>>> 80869d7d97ee728fb435aff2765d8964500829cb
 
       result = self.collection.update_one({'_id': ObjectId(_id)}, {'$set': update_data})
       return result
 
-<<<<<<< HEAD
- def delete_book(self, _id):
-
-    result = self.collection.delete_one({'_id': ObjectId(_id)})
-    return result
-
-=======
         Returns:
             dict: The book data if found, None otherwise.
         """
@@ -104,4 +72,3 @@ class BookRepository:
         """
         result = self.collection.delete_one({'_id': ObjectId(_id)})
         return result
->>>>>>> 80869d7d97ee728fb435aff2765d8964500829cb
